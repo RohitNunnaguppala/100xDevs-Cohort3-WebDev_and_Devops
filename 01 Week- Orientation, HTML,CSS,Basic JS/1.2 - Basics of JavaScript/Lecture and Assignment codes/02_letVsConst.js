bd8cbc77e34fa79335fca92c480ce2943495548c
RohitNunnaguppala <151500766+RohitNunnaguppala@users.noreply.github.com>
2024-12-17 00:50:41 +0530

    Create 02_letVsConst.js

---
+++ 01 Week- Orientation, HTML,CSS,Basic JS/1.2 - Basics of JavaScript/Lecture and Assignment codes/02_letVsConst.js
@@ -0,0 +1,15 @@
+// let, const
+
+// let
+let isStudent = true;
+
+console.log(isStudent); // true
+isStudent = false;
+console.log(isStudent); // false
+
+// const
+const firstName = "Rohit";
+
+console.log("Hello from before"); // Hello from before
+// firstName = 1;   // TypeError: Assignment to constant variable.
+console.log("Hello from after");
