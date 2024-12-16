bece8415543d6e96ab574e44751c6a6b6bc24663
RohitNunnaguppala <151500766+RohitNunnaguppala@users.noreply.github.com>
2024-12-17 00:54:29 +0530

    Create 03_datatypes_and_operators.js

---
+++ 01 Week- Orientation, HTML,CSS,Basic JS/1.2 - Basics of JavaScript/Lecture and Assignment codes/03_datatypes_and_operators.js
@@ -0,0 +1,43 @@
+let number = 44; // Number
+let string = "Hello World!"; // String
+let isActive = false; // Boolean
+let numbers = [1, 2, 3]; // Array
+
+console.log(number); // 44
+console.log(string); // Hello World!
+console.log(isActive); // false
+console.log(numbers); // [1, 2, 3]
+
+
+let user1 = "Harkirat";
+let user2 = "Bharat";
+let user3 = "Deepak";
+
+console.log(user1); // Harkirat
+console.log(user2); // Bharat
+console.log(user3); // Deepak
+
+
+// Array
+let users = ["Harkirat", "Bharat", "Deepak"];
+
+console.log(users); // ["Harkirat", "Bharat", "Deepak"]
+
+console.log(users[0]); // Harkirat
+console.log(users[1]); // Bharat
+console.log(users[2]); // Deepak
+console.log(users[3]); // undefined
+
+let sum = 10 + 20; // Arithmetic Operator
+let isEquals = (10 === 10); // Comparison Operator
+let isTrue = (true && false); // Logical Operator
+
+console.log(sum); // 30
+console.log(isEquals); // true
+console.log(isTrue); // false
+
+
+let age = 19;
+let canVote = (age >= 18); // true, false
+
+console.log(canVote); // true
