d453d7fdf69aca873d6ce6314a2f74bcefabf645
RohitNunnaguppala <151500766+RohitNunnaguppala@users.noreply.github.com>
2024-12-17 00:43:30 +0530

    Update 01_variable.js

--- 01 Week- Orientation, HTML,CSS,Basic JS/1.2 - Basics of JavaScript/Lecture and Assignment codes/01_variable.js
+++ 01 Week- Orientation, HTML,CSS,Basic JS/1.2 - Basics of JavaScript/Lecture and Assignment codes/01_variable.js
@@ -1,1 +1,20 @@
 
+let firstName = "Rohit";
+console.log(firstName); // Rohit
+
+const age = 20;
+console.log(age); // 20
+
+var isStudent = true;
+console.log(isStudent); // true
+
+
+console.log("firstName"); // firstName
+console.log("Rohit"); // Rohit
+
+
+isStudent = "Deepak";
+console.log(isStudent); // Deepak
+
+isStudent = 10;
+console.log(isStudent); // 10
