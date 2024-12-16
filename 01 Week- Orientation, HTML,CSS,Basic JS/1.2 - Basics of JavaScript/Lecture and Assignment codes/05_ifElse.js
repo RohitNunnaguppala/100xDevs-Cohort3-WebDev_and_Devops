c5c3166667b5ae4d7db00e0eca481d0818803390
RohitNunnaguppala <151500766+RohitNunnaguppala@users.noreply.github.com>
2024-12-17 00:59:16 +0530

    Create 05_ifElse.js

---
+++ 01 Week- Orientation, HTML,CSS,Basic JS/1.2 - Basics of JavaScript/Lecture and Assignment codes/05_ifElse.js
@@ -0,0 +1,21 @@
+let age = 19;
+
+if (age >= 18) {
+    console.log("Yes, you can vote");
+} else {
+    console.log("No, you can't vote");
+}
+
+// 1. Write an if/else statement that checks if a number is even or odd. 
+// If it's even, print "The number is even." Otherwise, print "The number is odd."
+
+
+// declare a variable `num` and assign it a value of 5
+let num = 5;
+
+// if the number is divisible by 2, it's the number is even else it's odd
+if (num % 2 === 0) {
+    console.log("The number is even.");
+} else {
+    console.log("The number is odd.");
+}
