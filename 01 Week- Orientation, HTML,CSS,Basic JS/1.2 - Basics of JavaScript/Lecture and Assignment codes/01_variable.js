fb75481e2e8483bcd56fb98801fc0eb15f969a78
RohitNunnaguppala <151500766+RohitNunnaguppala@users.noreply.github.com>
2024-12-17 13:36:44 +0530

    Update 01_variable.js

--- 01 Week- Orientation, HTML,CSS,Basic JS/1.2 - Basics of JavaScript/Lecture and Assignment codes/01_variable.js
+++ 01 Week- Orientation, HTML,CSS,Basic JS/1.2 - Basics of JavaScript/Lecture and Assignment codes/01_variable.js
@@ -19,6 +19,37 @@ console.log(isStudent); // Deepak
 isStudent = 10;
 console.log(isStudent); // 10
 
+var firstname="rohit";
+var lastname="nunnaguppala";
+
+var fullname=firstname+lastname;
+
+let height=156;
+
+console.log("My name is " +[fullname]+ " and my height is " +[height] +"cm");
+
+
+var favmovie="devara";
+let age=20;
+const liketravel=true;
+
+console.log(favmovie);
+console.log(age);
+console.log(liketravel);
+
+
+let isStudent = true;
+console.log(isStudent); 
+console.log(typeof isStudent);
+
+isStudent = "Rohit";
+console.log(isStudent); 
+console.log(typeof isStudent); 
+
+isStudent = 95;
+console.log(isStudent); 
+console.log(typeof isStudent); 
+
 
 /*
 1. Create a variable for each of the following: your favorite color, your height in centimeters, 
