5290dc9cd8ffa7a2ec776615a016cc4730fba2fa
RohitNunnaguppala <151500766+RohitNunnaguppala@users.noreply.github.com>
2024-12-18 16:29:40 +0530

    Create 10_functionalArgumentApproach1.js

---
+++ 02 Week - Async JS/2.1 - Async JS/Lecture and Assignment codes/10_functionalArgumentApproach1.js
@@ -0,0 +1,19 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+console.log(sum(1, 2)); // 3
+
+console.log(multiply(2, 5)); // 10
